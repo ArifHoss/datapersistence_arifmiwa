0c0323b413709e625579d4331feefd47974ac841
Arif Hossain <hossaininfo@ymail.com>
2023-02-21 09:52:20 +0100

    Initial commit

---
+++ src/main/java/com/example/datapersistence_arifmiwa/DatapersistenceArifmiwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.datapersistence_arifmiwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapersistenceArifmiwaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatapersistenceArifmiwaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datapersistence_arifmiwa/DatapersistenceArifmiwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datapersistence_arifmiwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapersistenceArifmiwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
